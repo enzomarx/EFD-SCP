a3db5f19b22021dbff6b6c468ab864e86eb3c279
enzomarx <enzomarxsantos@gmail.com>
2024-07-24 22:01:23 -0300

    upd

---
+++ testes.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+###### Assinatura
+time.sleep(3)
+pyautogui.click(x=259, y=64)
+time.sleep(3)
+pyautogui.click(x=358, y=332)
+time.sleep(3)
+pyautogui.click(x=690, y=531)
+time.sleep(3)
+pyautogui.click(x=546, y=320)
+time.sleep(3)
+pyautogui.click(x=873, y=471)
+time.sleep(3)
+pyautogui.click(x=681, y=389)
+
+##### Transmissão
+time.sleep(3)
+pyautogui.click(x=319, y=67)
+time.sleep(3)
+pyautogui.click(x=482, y=334)
+time.sleep(5)
+pyautogui.click(x=685, y=530)
+time.sleep(3)
+pyautogui.click(x=728, y=429) # nao salvar recibo
+time.sleep(3)
